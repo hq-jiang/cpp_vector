3c96985447587a4459e83062cda14164f97ca113
Hanqiu Jiang <hanq.jiang@gmail.com>
2021-01-03 13:40:09 +0100

    test: Add sized constructor unit test

--- test_vector.cpp
+++ test_vector.cpp
@@ -9,6 +9,15 @@ TEST(CppVector, DefaultConstructor) {
   ASSERT_THROW(vector.get(0), std::invalid_argument);
 }
 
+TEST(CppVector, SizedConstructor) {
+  Vector vector(4);
+  ASSERT_EQ(vector.size(), 4);
+  ASSERT_EQ(vector.capacity(), 4);
+  for(int i=0; i<vector.size(); ++i) {
+    ASSERT_EQ(vector.get(i), 0);
+  }
+}
+
 int main(int argc, char **argv) {
     testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
